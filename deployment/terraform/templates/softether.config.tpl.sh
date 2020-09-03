0de5d9565b82fd66561337bc365c0f2c7fa34b31
Vladimir Samoylov <cageyv.dev@gmail.com>
2020-09-04 01:14:21 +0700

    add BridgeCreate tun mode, add DNS2 1.0.0.1 (some magic with 1.1.1.1 and 1.0.0.1), need to fix routes (maybe my client doesn't support it)

--- deployment/terraform/templates/softether.config.tpl.sh
+++ deployment/terraform/templates/softether.config.tpl.sh
@@ -3,11 +3,14 @@ sudo tee "${FILE_PATH}" > /dev/null <<EOL
 Hub DEFAULT
 UserCreate * /GROUP:none /REALNAME:"Radius" /NOTE:"Used for Radius/MFA Auth"
 UserList
+BridgeCreate DEFAULT /DEVICE:sevpn /TAP:yes
+BridgeDeviceList
+BridgeList
 RadiusServerSet localhost:1812 /SECRET:"${RADIUS_SECRET}" /RETRY_INTERVAL:10000
 RadiusServerGet
 SecureNatHostSet /MAC:none /IP:${DHCP_GW} /MASK:${DHCP_MASK}
 SecureNatHostGet
-DhcpSet /START:${DHCP_START} /END:${DHCP_END} /MASK:${DHCP_MASK} /EXPIRE:7200 /GW:${DHCP_GW} /DNS:${DHCP_DNS} /DOMAIN:none /LOG:yes /PUSHROUTE:"${PUSH_ROUTE}"
+DhcpSet /START:${DHCP_START} /END:${DHCP_END} /MASK:${DHCP_MASK} /EXPIRE:7200 /GW:${DHCP_GW} /DNS:${DHCP_DNS} /DNS2:1.0.0.1 /DOMAIN:none /LOG:yes /PUSHROUTE:"${PUSH_ROUTE}"
 DhcpGet
 IPsecEnable /L2TP:yes /L2TPRAW:no /ETHERIP:no /PSK:"${PSK}" /DEFAULTHUB:default
 IPsecGet
