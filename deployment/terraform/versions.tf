72df859f762aa0b88b6ef727b44afe8b4bd4da5f
Vladimir Samoylov <cageyv.dev@gmail.com>
2021-01-14 16:31:23 +0700

    terraform 0.14+, aws 3.23+, random 3+, template 2.2+

--- deployment/terraform/versions.tf
+++ deployment/terraform/versions.tf
@@ -1,9 +1,17 @@
 terraform {
-  required_version = ">= 0.12.0, < 0.14.0"
-
+  required_version = ">= 0.14"
   required_providers {
-    aws      = ">= 2.0, < 4.0"
-    template = "~> 2.0"
-    random   = "~> 2.0"
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.23"
+    }
+    template = {
+      source = "hashicorp/template"
+      version = "~> 2.2"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
 }
