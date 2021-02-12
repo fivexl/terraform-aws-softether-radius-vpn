6c7000292d5d4549ee4e68f3d06435b5461c688b
Vladimir Samoylov <cageyv.dev@gmail.com>
2021-02-13 01:05:03 +0700

    terraform: update vpn_complete example

--- deployment/terraform/examples/vpn_complete/main.tf
+++ deployment/terraform/examples/vpn_complete/main.tf
@@ -1,9 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
-}
-
-data "aws_kms_key" "aws_ebs" {
-  key_id = "alias/aws/ebs"
+  region = "eu-central-1"
 }
 
 data "aws_availability_zones" "available" {
@@ -11,105 +7,89 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  project_name             = lower("vpn")
-  vpn_cidr                 = "10.78.0.0/24" # 10.78.0.1 - 10.78.0.254
-  vpc_cidr                 = "10.68.0.0/16" # 10.68.0.1 - 10.68.255.254
-  vpc_azs_max              = 3
-  vpc_public_subnets       = ["10.68.32.0/19", "10.68.64.0/19", "10.68.96.0/19"]
-  vpc_private_subnets      = ["10.68.128.0/19", "10.68.160.0/19", "10.68.192.0/19"]
-  vpc_database_subnets     = ["10.68.225.0/24", "10.68.226.0/24", "10.68.227.0/24"]
-  vpc_elasticache_subnets  = ["10.68.228.0/24", "10.68.229.0/24", "10.68.230.0/24"]
-  vpc_intra_subnets        = ["10.68.235.0/24", "10.68.236.0/24", "10.68.237.0/24"]
+  project_name       = lower("vpn")
+  vpc_cidr           = "10.68.0.0/16" # 10.68.0.1 - 10.68.255.254
+  vpc_azs_max        = 3
+  vpc_azs_list       = slice(data.aws_availability_zones.available.names, 0, tonumber(local.vpc_azs_max))
+  vpc_public_subnets = ["10.68.32.0/19", "10.68.64.0/19", "10.68.96.0/19"]
+}
+
+module "tag-generator" {
+  source            = "fivexl/tag-generator/aws"
+  version           = "1.0.1"
+  prefix            = local.project_name
+  terraform_managed = "1"
+  terraform_state   = "./terraform.tfstate"
 }
 
 module "vpc" {
-  source                                          = "terraform-aws-modules/vpc/aws"
-  version                                         = "2.64.0"
-  name                                            = local.project_name
-  cidr                                            = local.vpc_cidr
-  azs                                             = slice(data.aws_availability_zones.available.names, 0, tonumber(local.vpc_azs_max))
-  public_subnets                                  = local.vpc_public_subnets
-  private_subnets                                 = local.vpc_private_subnets
-  database_subnets                                = local.vpc_database_subnets
-  elasticache_subnets                             = local.vpc_elasticache_subnets
-  intra_subnets                                   = local.vpc_intra_subnets
-  enable_dns_hostnames                            = true
-  enable_dns_support                              = true
-  create_database_subnet_group                    = true
-  enable_nat_gateway                              = true
-  single_nat_gateway                              = true
-  one_nat_gateway_per_az                          = true
-  enable_vpn_gateway                              = false
-  manage_default_security_group                   = true
-  default_security_group_name                     = "default-${local.project_name}"
-  default_security_group_ingress                  = []
-  default_security_group_egress                   = []
-  enable_s3_endpoint                              = false
-  enable_public_s3_endpoint                       = false
-  enable_flow_log                                 = true
-  create_flow_log_cloudwatch_log_group            = true
-  create_flow_log_cloudwatch_iam_role             = true
-  flow_log_max_aggregation_interval               = 60
-  flow_log_cloudwatch_log_group_retention_in_days = 60
-  vpc_flow_log_tags = {
-    Name = "vpc-flow-logs-cloudwatch-logs-${local.project_name}"
-  }
+  source                         = "terraform-aws-modules/vpc/aws"
+  version                        = "2.70.0"
+  name                           = local.project_name
+  cidr                           = local.vpc_cidr
+  azs                            = local.vpc_azs_list
+  public_subnets                 = local.vpc_public_subnets
+  manage_default_security_group  = true
+  default_security_group_name    = "default-${local.project_name}"
+  default_security_group_ingress = []
+  default_security_group_egress  = []
+  tags                           = module.tag-generator.result
 }
 
 module "srvpn" {
-  source                  = "../../"
-  name                    = "softether-radius-vpn"
-  create_logs             = true
-  log_retention_days      = "1"
-  create_dns              = true
-  dns_zone_name           = "cageyv.com"
-  dns_a_record            = "vpn"
-  create_private_dns_zone = true
-  private_dns_zone_name   = "internal"
-  ami_name_prefix         = "softether-radius-vpn"
-  ami_owner               = "self"
-  vpc_id                  = module.vpc.vpc_id
-  public_subnet_tags      = { "Name" : "*-public-*" }
-  instance_type           = "t2.micro"
-  spot_price              = "0.0035"
-  key_pair_name           = ""
-  ebs_encrypt             = true
-  root_block_kms_key_arn  = data.aws_kms_key.aws_ebs.arn
-  target_cidr             = local.vpc_cidr
-  vpn_cidr                = local.vpn_cidr
-  vpn_admin_port          = "5555"
-  ldap_addr               = "ldaps://ldap.jumpcloud.com:636"
-  ldap_user_dn            = "uid={{username}},ou=users,o=XXXXXXXXXXXXXX,dc=jumpcloud,dc=com"
-  duo_enabled             = false
-  duo_ikey                = ""
-  duo_skey                = ""
-  duo_api_host            = ""
-  tags = {
-    "Project" : "softether-radius-vpn",
-    "Terraform" : "true"
-  }
+  source                         = "../../"
+  name                           = "softether-radius-vpn"
+  ami_name_prefix                = "softether-radius-vpn"
+  ami_owner                      = "self"
+  azs                            = [module.vpc.azs[0]]
+  subnets                        = [module.vpc.public_subnets[0]]
+  vpc_id                         = module.vpc.vpc_id
+  target_cidr                    = module.vpc.vpc_cidr_block
+  ldap_addr                      = "ldaps://ldap.jumpcloud.com:636"
+  ldap_user_dn                   = "uid={{username}},ou=users,o=XXXXXXXXXXXXXX,dc=jumpcloud,dc=com"
+  tags                           = module.tag-generator.result
+  asg_tags                       = module.tag-generator.result_asg_list
+  create_logs                    = true
+  log_retention_days             = 7
+  create_dns                     = false
+  create_private_dns_zone        = true
+  private_domain_fqdn            = "internal.fivexl.cloud"
+  instance_type                  = "t2.micro"
+  enable_detailed_monitoring     = false
+  enable_spot_instance           = true
+  enable_session_manager_connect = true
+  vpn_cidr                       = "10.78.0.0/24" # 10.78.0.1 - 10.78.0.254
+  vpn_dhcp_start                 = 10
+  vpn_dhcp_end                   = 200
+  vpn_admin_port                 = "5555"
 }
 
-output "this_vpn_dns_fqdn" {
-  value = module.srvpn.this_vpn_dns_fqdn
+output "srvpn_vpn_dns_fqdn_list" {
+  description = "DNS names of created VPN instance"
+  value       = module.srvpn.this_vpn_dns_fqdn_list
 }
 
-output "this_vpn_private_dns_zone_name" {
-  value = module.srvpn.this_vpn_private_dns_zone_name
+output "srvpn_vpn_public_ip" {
+  description = "The Public IP of created VPN instance"
+  value       = module.srvpn.this_vpn_public_ip
 }
 
-output "this_vpn_public_ip" {
-  value = module.srvpn.this_vpn_public_ip
+output "srvpn_vpn_ipsec_psk" {
+  description = "Pre-shared Key for VPN clients"
+  value       = module.srvpn.this_vpn_ipsec_psk
 }
 
-output "this_vpn_ipsec_psk" {
-  value     = module.srvpn.this_vpn_ipsec_psk
+output "srvpn_vpn_server_password" {
+  description = "VPN server admin password"
+  value       = module.srvpn.this_vpn_server_password
 }
 
-output "this_vpn_server_password" {
-  value     = module.srvpn.this_vpn_server_password
+output "srvpn_vpn_push_route" {
+  description = "Route for VPN clients if they can't get DHCP Classless Static Routes"
+  value       = module.srvpn.this_vpn_push_route
 }
 
-output "this_vpn_push_route" {
-  value = module.srvpn.this_vpn_push_route
+output "srvpn_security_group_id" {
+  description = "VPN Security Group ID"
+  value       = module.srvpn.this_security_group_id
 }
